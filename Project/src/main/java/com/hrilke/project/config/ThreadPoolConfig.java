371a81cbb6aca20672e112bc6451a0cda0480c1d
hth19 <hth19@TAEHWA>
2023-03-22 12:03:38 +0900

    ThreadPool 비동기처리 공부 3/22

--- Project/src/main/java/com/hrilke/project/config/ThreadPoolConfig.java
+++ Project/src/main/java/com/hrilke/project/config/ThreadPoolConfig.java
@@ -2,7 +2,6 @@ package com.hrilke.project.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import com.hrilke.project.service.concurrent.AsyncService;
--- Project/src/main/java/com/hrilke/project/controller/concurrent/AsyncController.java
+++ Project/src/main/java/com/hrilke/project/controller/concurrent/AsyncController.java
@@ -1,6 +1,5 @@
 package com.hrilke.project.controller.concurrent;
 
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
